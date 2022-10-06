7d59322ba694fb6c9a6ace2c4f72d08caaebc371
john subba <senma777@gmail.com>
2022-10-06 23:00:38 +0545

    updates on home page

---
+++ src/components/Body/About.js
@@ -0,0 +1,40 @@
+import React from "react";
+import ProfileImg from "../../assets/prof.jpeg";
+
+const About = () => {
+  return (
+    <div className="grid grid-cols-2 gap-6 items-center">
+      <div className="p-24">
+        <img src={ProfileImg} alt="profile" />
+      </div>
+      <div>
+        <div className="pb-6 text-4xl">
+          About <span className="bold">Me</span>
+        </div>
+        <div className="text-zinc-400 text-sm pb-5">
+          Diving into the world of programming without any educational
+          background on IT fields was the first challenge I faced. Self learning
+          from online platforms and frustation from code not working while
+          learning was what I faced while walking down the path of coding.
+          <br />
+          <span className="pl-12">
+            But all these hardwork has lead me to a successfull start as
+            Frontend-Dev(ReactJS). And I'm happy to see the journey I've
+            completed till now. I'm looking forward to use all of my
+            skills/experience in the company where my skills are appreciated.
+          </span>
+        </div>
+        <div className="flex gap-3">
+          <button className="btn text-white hover:text-black bg-zinc-700 hover:bg-white">
+            Hire Me
+          </button>
+          <button className="btn text-black hover:text-white bg-zinc-200 hover:bg-zinc-700">
+            Download CV
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Body/Skills.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { FaJs, FaReact, FaGit } from "react-icons/fa";
+import { SiTailwindcss, SiReduxsaga, SiRedux, SiWebpack } from "react-icons/si";
+
+const skillsArray = [
+  { icons: <FaJs />, name: "Javascript", color: "text-yellow-400" },
+  { icons: <FaReact />, name: "ReactJS", color: "text-blue-400" },
+  { icons: <SiTailwindcss />, name: "TailwindCSS", color: "text-blue-600" },
+  { icons: <SiRedux />, name: "Redux", color: "text-indigo-500" },
+  { icons: <SiReduxsaga />, name: "Redux-Saga", color: "text-emerald-400" },
+  { icons: <FaGit />, name: "Git", color: "text-amber-600" },
+  { icons: <SiWebpack />, name: "Webpack", color: "text-blue-300" },
+];
+
+const Skills = () => {
+  return (
+    <div className="my-10 mt-16">
+      <div className="flex justify-center gap-2 items-baseline pb-10">
+        <span className="text-2xl">Featured</span>
+        <span className="text-2xl semi-bold">Skills</span>
+      </div>
+      <div className="flex w-full justify-evenly items-center mb-10 mt-4">
+        {skillsArray.map((each, index) => {
+          return (
+            <div className="flex flex-col items-center gap-4" key={index}>
+              <div
+                className={`${each.color} hover:scale-125 duration-300 ease-in-out cursor-pointer text-7xl`}
+              >
+                {each.icons}
+              </div>
+              <div>{each.name}</div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
