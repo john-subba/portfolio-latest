be2972ec62401acc6e9e874c9484d01302ad505f
john subba <senma777@gmail.com>
2022-10-07 22:27:02 +0545

    updates on educations/exp page

--- src/components/Body/Educations.js
+++ src/components/Body/Educations.js
@@ -3,18 +3,46 @@ import { FaCalendarAlt } from "react-icons/fa";
 
 const Educations = () => {
   return (
-    <div className="my-10 mt-20">
+    <div className="my-10 mt-40">
       <div className="flex justify-center gap-2 items-baseline pb-10">
         <span className="text-2xl">My</span>
-        <span className="text-2xl semi-bold">Resume</span>
+        <span className="text-2xl semi-bold">Work Experience</span>
       </div>
       <div className="grid grid-cols-2 gap-8">
-        <div>
-          <div className="flex items-center gap-2 text-sm">
+        <div className="p-8 bg-zinc-800 rounded-md">
+          <div className="flex items-center gap-2 text-sm pb-4">
             <FaCalendarAlt /> <span>March 2022 - June 2022</span>
           </div>
-          <div className="">React Developer Internship</div>
-          <div>WaftTech Pvt. Ltd.</div>
+          <div className="pb-4 text-xl text-amber-600">
+            React Developer Internship
+          </div>
+          <div className="text-sm text-zinc-400 pb-2">
+            Learned to work around demo projects using React as base block,
+            Redux for state management, Redux-Saga for API management.
+          </div>
+          <div className="text-sm text-zinc-400 pb-2">
+            Worked in Tailwind CSS for styling and to create custom UI
+            components.
+          </div>
+          <div className="mt-4 text-zinc-300">WaftTech Pvt. Ltd.</div>
+        </div>
+        <div className="p-8 bg-zinc-800 rounded-md">
+          <div className="flex items-center gap-2 text-sm pb-4">
+            <FaCalendarAlt /> <span>June 2022 - Current</span>
+          </div>
+          <div className="pb-4 text-xl text-amber-600">
+            Junior React Developer
+          </div>
+          <div className="text-sm text-zinc-400 pb-2">
+            Currently working in different ecommerce related projects such as
+            saathimart.com, socheko.com, upakart.com & sybazzar.com.
+          </div>
+          <div className="text-sm text-zinc-400 pb-2">
+            Also working in optimizing these projects and also working on
+            waftcommerce project which can be used to create a ecommerce
+            platform in instant.
+          </div>
+          <div className="mt-4 text-zinc-300">WaftTech Pvt. Ltd.</div>
         </div>
       </div>
     </div>
