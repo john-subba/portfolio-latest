6552501298f11d8ca2add8d0a21f9edaf7bd5a3c
john subba <senma777@gmail.com>
2022-10-08 10:34:22 +0545

    updates on snackbar and form data

--- src/components/Body/Contact.js
+++ src/components/Body/Contact.js
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Contact = () => {
   const [formData, setFormData] = useState({
-    full_name: "",
+    name: "",
     email: "",
-    contact_no: "",
     message: "",
   });
   const [formError, setFormError] = useState({
-    full_name: "",
+    name: "",
     email: "",
-    contact_no: "",
     message: "",
   });
 
@@ -21,19 +21,13 @@ const Contact = () => {
 
   const validateData = (data) => {
     let validate = true;
-    if (data.full_name === "") {
-      setFormError({ ...formError, full_name: "Please add some value." });
+    if (data.name === "") {
+      setFormError({ ...formError, name: "Please add some value." });
       validate = false;
-    }
-    if (data.email === "") {
+    } else if (data.email === "") {
       setFormError({ ...formError, email: "Please add some value." });
       validate = false;
-    }
-    if (data.contact_no === "") {
-      setFormError({ ...formError, contact_no: "Please add some value." });
-      validate = false;
-    }
-    {
+    } else if (data.message === "") {
       setFormError({ ...formError, message: "Please add some value." });
       validate = false;
     }
@@ -43,95 +37,127 @@ const Contact = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (validateData(formData) === true) {
-      console.log("submitted");
+      const formData = new FormData();
+      formData.append(
+        "name",
+        document.querySelector('input[name="name"]').value
+      );
+      formData.append(
+        "email",
+        document.querySelector('input[name="email"]').value
+      );
+      formData.append(
+        "message",
+        document.querySelector('textarea[name="message"]').value
+      );
+
+      fetch("https://getform.io/f/83db24c9-4e7d-4971-a76b-2a3e661a719b", {
+        method: "POST",
+        body: formData,
+      })
+        .then(
+          (response) =>
+            response.status === 200 &&
+            toast.success("Thank you for submitting the form.", {
+              position: "bottom-left",
+              autoClose: 5000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+            })
+        )
+        .catch((error) => console.log(error));
     } else {
-      console.log("not submitted");
+      toast.warn("Please add all required fields.", {
+        position: "bottom-left",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
     }
   };
 
   return (
-    <div className="my-10 mt-40">
-      <div className="flex justify-center gap-2 items-baseline pb-10">
-        <span className="text-2xl">Get</span>
-        <span className="text-2xl semi-bold">In Touch</span>
-      </div>
-      <div className="grid grid-cols-2 gap-20">
-        <div className="">
-          <div className="mb-8 text-xl">Send Me Your Details</div>
-          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-            <input
-              type="text"
-              name="full_name"
-              placeholder="Your Full Name"
-              className={`${
-                formError.full_name !== "" ? "error" : ""
-              } px-4 py-3 rounded bg-zinc-700 text-sm medium-bold focus:bg-zinc-600 outline-none`}
-              onChange={handleChange}
-              value={formData.full_name}
-            />
-            <input
-              type="email"
-              name="email"
-              placeholder="Your Email Address"
-              className={`${
-                formError.email !== "" ? "error" : ""
-              } px-4 py-3 rounded bg-zinc-700 text-sm medium-bold focus:bg-zinc-600 outline-none`}
-              onChange={handleChange}
-              value={formData.email}
-            />
-            <input
-              type="number"
-              name="contact_no"
-              placeholder="Your Contact No."
-              className={`${
-                formError.contact_no !== "" ? "error" : ""
-              } px-4 py-3 rounded bg-zinc-700 text-sm medium-bold focus:bg-zinc-600 outline-none`}
-              onChange={handleChange}
-              value={formData.contact_no}
-            />
-            <textarea
-              placeholder="Write a Message"
-              name="message"
-              className={`${
-                formError.message !== "" ? "error" : ""
-              } px-4 py-3 rounded bg-zinc-700 text-sm medium-bold focus:bg-zinc-600 outline-none h-44`}
-              onChange={handleChange}
-              value={formData.message}
-            />
-            <button
-              type="submit"
-              className="btn text-white hover:text-black bg-zinc-700 hover:bg-white w-1/4"
-            >
-              Send
-            </button>
-          </form>
+    <>
+      <div className="my-10 mt-40">
+        <div className="flex justify-center gap-2 items-baseline pb-10">
+          <span className="text-2xl">Get</span>
+          <span className="text-2xl semi-bold">In Touch</span>
         </div>
-        <div>
-          <div className="mb-8 text-xl">My Contact Details</div>
-          <div className="pb-4">
-            <div className="text-sm tracking-wider text-blue-700 pb-2">
-              EMAIL
-            </div>
-            <div className="text-sm text-zinc-400">senma777@gmail.com</div>
-          </div>
-          <div className="pb-4">
-            <div className="text-sm tracking-wider text-blue-700 pb-2">
-              CONTACT NO.
-            </div>
-            <div className="text-sm text-zinc-400">+977-9849786283</div>
+        <div className="grid grid-cols-2 gap-20">
+          <div className="">
+            <div className="mb-8 text-xl">Send Me Your Details</div>
+            <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+              <input
+                type="text"
+                name="name"
+                placeholder="Your Full Name"
+                className={`${
+                  formError.name !== "" ? "error" : ""
+                } px-4 py-3 rounded bg-zinc-700 text-sm medium-bold focus:bg-zinc-600 outline-none`}
+                onChange={handleChange}
+                value={formData.name}
+              />
+              <input
+                type="email"
+                name="email"
+                placeholder="Your Email Address"
+                className={`${
+                  formError.email !== "" ? "error" : ""
+                } px-4 py-3 rounded bg-zinc-700 text-sm medium-bold focus:bg-zinc-600 outline-none`}
+                onChange={handleChange}
+                value={formData.email}
+              />
+              <textarea
+                placeholder="Write a Message"
+                name="message"
+                className={`${
+                  formError.message !== "" ? "error" : ""
+                } px-4 py-3 rounded bg-zinc-700 text-sm medium-bold focus:bg-zinc-600 outline-none h-44`}
+                onChange={handleChange}
+                value={formData.message}
+              />
+              <button
+                type="submit"
+                className="btn text-white hover:text-black bg-zinc-700 hover:bg-white w-1/4"
+              >
+                Send
+              </button>
+            </form>
           </div>
           <div>
-            <div className="text-sm tracking-wider text-blue-700 pb-2">
-              ADDRESS
+            <div className="mb-8 text-xl">My Contact Details</div>
+            <div className="pb-4">
+              <div className="text-sm tracking-wider text-blue-700 pb-2">
+                EMAIL
+              </div>
+              <div className="text-sm text-zinc-400">senma777@gmail.com</div>
             </div>
-            <div className="text-sm text-zinc-400">
-              <div>Bhaisepati, Lalitpur 25</div>
-              <div>Nepal</div>
+            <div className="pb-4">
+              <div className="text-sm tracking-wider text-blue-700 pb-2">
+                CONTACT NO.
+              </div>
+              <div className="text-sm text-zinc-400">+977-9849786283</div>
+            </div>
+            <div>
+              <div className="text-sm tracking-wider text-blue-700 pb-2">
+                ADDRESS
+              </div>
+              <div className="text-sm text-zinc-400">
+                <div>Bhaisepati, Lalitpur 25</div>
+                <div>Nepal</div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      <ToastContainer />
+    </>
   );
 };
 
